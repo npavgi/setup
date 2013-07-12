93463c160be5d27be2691b189daba5a53268e89b
npavgi <narayanpavgi@gmail.com>
2013-07-12 16:00:57 -0500

    .emacs points to init.el
    Noticed that certain new custom configurations would work in emacs when present in .emacs but not when present in init.el - even when init.el was getting being loaded. Some existing configurations had already been coded in init.el. So instead of splitting configurations over 2 files, linked one to the other.

--- setup.sh
+++ setup.sh
@@ -37,10 +37,10 @@ fi
 if [ -d .emacs.d/ ]; then
     mv .emacs.d .emacs.d~
 fi
-git clone https://github.com/startup-class/dotfiles.git
+git clone https://github.com/npavgi/dotfiles.git
 ln -sb dotfiles/.screenrc .
 ln -sb dotfiles/.bash_profile .
 ln -sb dotfiles/.bashrc .
 ln -sb dotfiles/.bashrc_custom .
 ln -sf dotfiles/.emacs.d .
-
+ln -s  .emacs.d/init.el .emacs
